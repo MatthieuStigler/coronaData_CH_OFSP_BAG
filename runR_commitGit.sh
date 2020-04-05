b7ce647b504bd5c9215e8d30660ceff6fce5858f
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2020-04-04 17:00:13 -0700

    Add bash script

---
+++ runR_commitGit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+## Run R
+#Rscript --vanilla code/0_1_download_BAG_OFSP.R #> /dev/null 2>&1 &
+#Rscript --vanilla code/0_2_docs_process.R 
+#Rscript --vanilla code/0_3_check_data.R 
+#Rscript --vanilla code/0_6_visualize_plots.R 
+rm -f Rplots.pdf
+
+## GIT stuff
+
+git_mess="Automatic update, $(date +'%d %b %Y')"
+git status
+# git add data_raw/OFSP_report_downloaded*
+# git commit --all -m "$git_mess"
+# 
+echo "Done"
+
